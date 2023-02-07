a1a153c0100b8649e2b7cd52dd1953815b8bc939
Kauan Rodrigues Machado <182200283@senacinformatica.br>
2023-02-07 17:32:17 -0300

    kk

--- validador.js
+++ validador.js
@@ -76,11 +76,67 @@ function mascaraCNPJ(i) {
 
 function validar() {
     var nome = FormUser.nome.value;
+    var email = FormUser.email.value;
+    var tel  = FormUser.fone.value;
+    var endereco = FormUser.end.value;
+    var tipoCPF = FormUser.cpf.value;
+    var tipoCNPJ = FormUser.cnpj.value;
+    var senha = FormUser.senha.value;
+    var confSenha = FormUser.senha.value;
     
+
     if(nome == "" || nome.length <= 3 ){
         alert("informe o nome completo");
         FormUser.nome.focus();
         return false;
     }
+    if( email  == "" || email.length <= 10 ){
+        alert("informe o email correto");
+        return false;
+    }
+    if( tel  == "" || tel.length <= 11 ){
+        alert("informe o tel correto");
+        FormUser.fone.focus();
+        return false;
+    }
+    if(endereco == "" || end.length <= 11 ){
+        alert("informe o endereço completo");
+        FormUser.tpPessoa.focus();
+        return false;
+    }
+    if(endereco == "" || endereco.length <= 11 ){
+        alert("informe o endereço completo");
+        FormUser.end.focus();
+        return false;
+    }
+    if(tipoCPF == "" || tipoCNPJ == ""){
+        alert("selecione um documento");
+        return false;
+    }
+    if(tipoCPF != "" && tipoCNPJ == ""){
+        if(tipoCPF.length != 14){
+            alert("CPF incorreto!");
+            FormUser.cpf.focus();
+            return false;
+        }
+        if(tipoCNPJ != "" && tipoCPF == ""){
+            if(tipoCNPJ.length != 18){
+                alert("CNPJ incorreto!");
+                FormUser.cnpj.focus();
+                return false;
+            }
+    }
+      
+}
+if(senha == "" || senha.length <= 3 ){
+    alert("senha não atende aos requisitos");
+    FormUser.senha.focus();
+    return false;
+}
+if(confSenha != confSenha) {
+    alert("senhas não conferem");
+    FormUser.senha.focus();
+    return false;
+}
 }
        
